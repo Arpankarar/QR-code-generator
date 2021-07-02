a9e037c8a09d5e08b4109eb27e4e6a6ee0a49fd6
unknown <you@example.com>
2021-07-02 12:40:38 +0530

    front end completed

--- Qr_generator.py
+++ Qr_generator.py
@@ -6,10 +6,39 @@ class Qr_Generator:
         self.root.title("QR-Generator | by Arpan Karar")
         self.root.resizable(False,False)
         
-        title=Label(self.root,text= "   Qr Code Generator" , font =("times roman",50),bg="#BEAEE2",fg="#053742",anchor="w").place(x=0,y=0,relwidth=1)
+        title=Label(self.root,text= "   Qr Code Generator" , font =("times new roman",50),bg="#BEAEE2",fg="#053742",anchor="w").place(x=0,y=0,relwidth=1)
         #============= employee details window ==========
-        emp_Frame = Frame(self.root,bd=2,relief=RIDGE)
+        emp_Frame = Frame(self.root,bd=2,relief=RIDGE,bg='white')
         emp_Frame.place(x=50,y=100,width=500,height=380)
+        emp_title=Label(emp_Frame,text= " Employee Details" , font =("goudy old style",30),fg="#053742").place(x=0,y=0,relwidth=1)
+        
+        lbl_emp_code=Label(emp_Frame,text= " Employee ID" , font =("goudy old style",15,'bold'),bg="white").place(x=20,y=60)
+        lbl_name=Label(emp_Frame,text= " Name" , font =("goudy old style",15,'bold'),bg="white").place(x=20,y=100)
+        lbl_department=Label(emp_Frame,text= " Department" , font =("goudy old style",15,'bold'),bg="white").place(x=20,y=140)
+        lbl_designation=Label(emp_Frame,text= " Designation" , font =("goudy old style",15,'bold'),bg="white").place(x=20,y=180)
+        
+        txt_emp_code=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=60)
+        txt_name=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=100)
+        txt_department=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=140)
+        txt_designation=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=180)
+        
+        
+        btn_generator = Button(emp_Frame,text="QR Generator",font=("times new roman",18,'bold'),bg="#0A1931",fg="white").place(x=80,y=250,width=200,height=40)
+        btn_clear = Button(emp_Frame,text="Clear",font=("times new roman",18,'bold'),bg="#4C4C6D",fg="white").place(x=290,y=250,width=150,height=40)
+        
+        self.msg="QR Generated Successfully !!!"
+        self.lbl_msg=Label(emp_Frame,text=self.msg,font=("times new roman",15),bg="white",fg="green").place(x=0,y=300,relwidth=1)
+        
+        #    ==================Qr code frame=======================
+        qr_Frame = Frame(self.root,bd=2,relief=RIDGE,bg='white')
+        qr_Frame.place(x=600,y=100,width=250,height=380)
+        qr_title=Label(qr_Frame,text= " Employee QR code" , font =("goudy old style",22),fg="#053742",anchor="w").place(x=0,y=0,relwidth=1)
+        
+        self.qr_code=Label(qr_Frame,text="QR code \nNot Available.",font=("times new roman",15),bg="#FFF5DA",fg="#053742",bd=1,relief=RIDGE)
+        self.qr_code.place(x=35,y=100,width=180,height=180)
+
+    
+        
 root=Tk()
 obj =Qr_Generator(root)
 root.mainloop()
