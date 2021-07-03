a37171256e488eeac8e70620ba0cd094f8d7c631
unknown <you@example.com>
2021-07-03 10:51:14 +0530

    validator added and input data collected

--- Qr_generator.py
+++ Qr_generator.py
@@ -8,6 +8,16 @@ class Qr_Generator:
         
         title=Label(self.root,text= "   Qr Code Generator" , font =("times new roman",50),bg="#BEAEE2",fg="#053742",anchor="w").place(x=0,y=0,relwidth=1)
         #============= employee details window ==========
+        
+        
+        #============== variables =====================
+        
+        self.var_emp_code = StringVar()
+        self.var_name = StringVar()
+        self.var_department = StringVar()
+        self.var_designation = StringVar()
+        
+        
         emp_Frame = Frame(self.root,bd=2,relief=RIDGE,bg='white')
         emp_Frame.place(x=50,y=100,width=500,height=380)
         emp_title=Label(emp_Frame,text= " Employee Details" , font =("goudy old style",30),fg="#053742").place(x=0,y=0,relwidth=1)
@@ -17,17 +27,18 @@ class Qr_Generator:
         lbl_department=Label(emp_Frame,text= " Department" , font =("goudy old style",15,'bold'),bg="white").place(x=20,y=140)
         lbl_designation=Label(emp_Frame,text= " Designation" , font =("goudy old style",15,'bold'),bg="white").place(x=20,y=180)
         
-        txt_emp_code=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=60)
-        txt_name=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=100)
-        txt_department=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=140)
-        txt_designation=Entry(emp_Frame, font =("goudy old style",15),bg="#EDF6E5").place(x=200,y=180)
+        txt_emp_code=Entry(emp_Frame, font =("goudy old style",15),textvariable=self.var_emp_code,bg="#EDF6E5").place(x=200,y=60)
+        txt_name=Entry(emp_Frame, font =("goudy old style",15),textvariable=self.var_name,bg="#EDF6E5").place(x=200,y=100)
+        txt_department=Entry(emp_Frame, font =("goudy old style",15),textvariable=self.var_department,bg="#EDF6E5").place(x=200,y=140)
+        txt_designation=Entry(emp_Frame, font =("goudy old style",15),textvariable=self.var_designation,bg="#EDF6E5").place(x=200,y=180)
         
         
-        btn_generator = Button(emp_Frame,text="QR Generator",font=("times new roman",18,'bold'),bg="#0A1931",fg="white").place(x=80,y=250,width=200,height=40)
-        btn_clear = Button(emp_Frame,text="Clear",font=("times new roman",18,'bold'),bg="#4C4C6D",fg="white").place(x=290,y=250,width=150,height=40)
+        btn_generator = Button(emp_Frame,text="QR Generator",command=self.generate,font=("times new roman",18,'bold'),bg="#0A1931",fg="white").place(x=80,y=250,width=200,height=40)
+        btn_clear = Button(emp_Frame,text="Clear",command=self.clear,font=("times new roman",18,'bold'),bg="#4C4C6D",fg="white").place(x=290,y=250,width=150,height=40)
         
-        self.msg="QR Generated Successfully !!!"
-        self.lbl_msg=Label(emp_Frame,text=self.msg,font=("times new roman",15),bg="white",fg="green").place(x=0,y=300,relwidth=1)
+        self.msg=""
+        self.lbl_msg=Label(emp_Frame,text=self.msg,font=("times new roman",15),bg="white",fg="green")
+        self.lbl_msg.place(x=0,y=300,relwidth=1)
         
         #    ==================Qr code frame=======================
         qr_Frame = Frame(self.root,bd=2,relief=RIDGE,bg='white')
@@ -36,7 +47,22 @@ class Qr_Generator:
         
         self.qr_code=Label(qr_Frame,text="QR code \nNot Available.",font=("times new roman",15),bg="#FFF5DA",fg="#053742",bd=1,relief=RIDGE)
         self.qr_code.place(x=35,y=100,width=180,height=180)
-
+        
+    def clear(self):
+        self.var_emp_code.set('')
+        self.var_name.set('')
+        self.var_department.set('')
+        self.var_designation.set('')
+        self.msg=""
+        self.lbl_msg.config(text=self.msg)
+    
+    def generate(self):
+        if self.var_emp_code.get()=='' or self.var_name.get()==''or self.var_department.get()=='' or self.var_designation.get()=='' :
+            self.msg='All Fields are required !!'
+            self.lbl_msg.config(text=self.msg,fg="red")
+        else:
+            self.msg="QR Generated Successfully !!"
+            self.lbl_msg.config(text=self.msg,fg="green")
     
         
 root=Tk()
